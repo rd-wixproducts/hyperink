11265cdf4592f72871869caf12006f669c57a8b1
Shelby Pefley <spefley@mit.edu>
2014-07-19 19:34:53 -0400

    Add page, make click markers handle resize

--- web/app.js
+++ web/app.js
@@ -1,1 +1,38 @@
 var app = angular.module('hyperink', []);
+
+app.controller('paperCtrl', function($scope) {
+  $scope.comments = [];
+  $scope.makingComment = false;
+  $scope.tempComment = {};
+
+  $scope.createComment = function($event) {
+    console.log($event);
+    $scope.makingComment = false;
+
+    $scope.tempComment = {
+      x: $event.pageX - paper.offsetLeft,
+      y: $event.pageY,
+      comment: ''
+    };
+
+    $scope.makingComment = true;
+
+    $scope.comments.push($scope.tempComment);
+  };
+
+  $scope.cmtStyle = function(x, y) {
+    var nX = x + paper.offsetLeft;
+    return {'left': nX + 'px', 'top': y + 'px'};
+  };
+  
+  window.onresize = function() {
+    $scope.$apply();
+  };
+});
+
+app.directive('paper', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'paper.partial.html'
+  };
+});
