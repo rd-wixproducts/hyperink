7c66ed031c3c15998608d55d2fa232abda2d3f5c
Anish Athalye <me@anishathalye.com>
2014-07-20 08:54:32 -0700

    Fix

--- web/app.js
+++ web/app.js
@@ -1,12 +1,15 @@
-var app = angular.module('hyperink', []);
+var app = angular.module('hyperink', ['firebase']);
 
-app.controller('paperCtrl', function($scope) {
-  $scope.comments = [];
+app.controller('paperCtrl', function($scope, $firebase) {
+  var ref = new Firebase('https://popping-fire-1233.firebaseIO.com');
+  $scope.comments = $firebase(ref);
+
   $scope.makingComment = false;
   $scope.tempComment = null;
   $scope.mousedCmt = null;
   $scope.showMousedCmt = false;
   $scope.curve = undefined;
+  $scope.tempKey = null;
 
   $scope.mouseDown = function($event) {
     var x = ($event.pageX - paper.offsetLeft),
@@ -17,7 +20,11 @@ app.controller('paperCtrl', function($scope) {
       minX: x, maxX: x,
       minY: y, maxY: y
     };
-    $scope.comments.push($scope.tempComment);
+    $scope.tempKey = null;
+    $scope.comments.$add($scope.tempComment).then(function(ref) {
+        $scope.tempKey = ref.name();
+        console.log($scope.tempKey);
+    });
   };
 
   $scope.mouseMove = function($event) {
@@ -30,6 +37,13 @@ app.controller('paperCtrl', function($scope) {
       $scope.tempComment.maxX = Math.max($scope.tempComment.maxX, x);
       $scope.tempComment.minY = Math.min($scope.tempComment.minX, y);
       $scope.tempComment.maxY = Math.max($scope.tempComment.maxY, y);
+      if (key !== null) {
+          var key = $scope.tempKey;
+          var newData = {};
+          newData[key] = $scope.tempComment;
+          console.log(newData);
+          $scope.comments.$update(newData);
+      }
     }
   };
 
@@ -39,6 +53,13 @@ app.controller('paperCtrl', function($scope) {
       $scope.tempComment.x = $scope.tempComment.minX / 2 + $scope.tempComment.maxX / 2;
       $scope.tempComment.y = $scope.tempComment.minY / 2 + $scope.tempComment.maxY / 2;
       $scope.makingComment = true;
+      if (key !== null) {
+          var key = $scope.tempKey;
+          var newData = {};
+          newData[key] = $scope.tempComment;
+          console.log(newData);
+          $scope.comments.$update(newData);
+      }
     }
   };
 
@@ -63,10 +84,18 @@ app.controller('paperCtrl', function($scope) {
   $scope.submitComment = function() {
     $scope.tempComment.text = $scope.commentText;
     //$scope.comments.push($scope.tempComment);
+    if (key !== null) {
+        var key = $scope.tempKey;
+        var newData = {};
+        newData[key] = $scope.tempComment;
+        console.log(newData);
+        $scope.comments.$update(newData);
+    }
     
     $scope.tempComment = null;
     $scope.commentText = '';
     $scope.makingComment = false;
+
   };
 
   $scope.closeCommentDialog = function() {
@@ -77,8 +106,13 @@ app.controller('paperCtrl', function($scope) {
 
   $scope.removeComment = function() {
     for(cmt in $scope.comments) {
+        console.log($scope.comments[cmt]);
+        if ($scope.comments[cmt] === null) continue;
       if($scope.comments[cmt].text === $scope.mousedCmt.text) {
-        $scope.comments.splice(cmt, 1);
+          console.log(cmt);
+          // console.log("name = " + $scope.comments[cmt].name());
+          $scope.comments.$remove(cmt);
+          // $scope.comments.splice(cmt, 1);
       }
     }
     $scope.makingComment = false;
